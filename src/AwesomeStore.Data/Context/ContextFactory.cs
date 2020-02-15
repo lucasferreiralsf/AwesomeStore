33d239d2b20336a4ad2c2caa1e4cb1dd38366a03
lucasferreiralsf <lucasferreiralsf@hotmail.com>
2020-02-15 01:45:08 -0300

    AwesomeStore.Data init

---
+++ src/AwesomeStore.Data/Context/ContextFactory.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AwesomeStore.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server=localhost;Port=3306;Database=awesome_store_db;Uid=root;Pwd=root";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ src/AwesomeStore.Data/Context/MyContext.cs
@@ -0,0 +1,21 @@
+using AwesomeStore.Data.Mapping;
+using AwesomeStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AwesomeStore.Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserEntity>(new UserMap().Configure);
+        }
+    }
+}
